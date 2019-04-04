91d49940275f7f82c75221186c67e10d960bb5b0
Kylie Lin <kylieclin@gmail.com>
2019-04-04 15:15:38 -0700

    make nav links dynamic

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,32 +1,33 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import SideNav from './sidenav';
 
 class Nav extends Component{
-
+    renderLinks(){
+        return(
+            <Fragment>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            </Fragment>
+        )
+    }
     render(){
+        const links = this.renderLinks();
+
         return (
         <div className="navbar-fixed">
-            <nav className="green darken-3">
+            <nav className="green darken-3 ">
                 <div className="nav-wrapper">
                     <Link className="brand-logo" to="/">Wicked Sales</Link>
                     <a href="#" data-target="sidenav" className="sidenav-trigger right">
                         <i className="material-icons">menu</i>
                     </a>
                     <ul className="right hide-on-med-and-down">
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/products">Products</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Sign In</Link>
-                        </li>
+                        {links}
                     </ul>
                 </div>
             </nav>
-            <SideNav />
+            <SideNav links={links}/>
         </div>
         )
     }
--- src/components/nav/sidenav.js
+++ src/components/nav/sidenav.js
@@ -3,23 +3,16 @@ import {Link} from 'react-router-dom';
 
 class SideNav extends Component{
     componentDidMount(){
-        console.log(this.sidenav);
-
         M.Sidenav.init(this.sidenav,{   inDuration: 350,
-            outDuration: 350, edge: 'right'});
+            outDuration: 350, 
+            edge: 'right',
+            background: 'transparent'
+        });
     }
     render(){
         return (
             <ul ref={(element)=>{this.sidenav = element}} id="sidenav" className="sidenav right">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-                <li>
-                    <Link to="/">Sign In</Link>
-                </li>
+                {this.props.links}
             </ul>
         )
     }
