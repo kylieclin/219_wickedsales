d2fd7ea5bd2e7e813eb850924cde38e24212a193
Kylie Lin <kylieclin@gmail.com>
2019-04-05 16:10:05 -0700

    budle for deploy

--- src/components/products/home.js
+++ src/components/products/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default props =>{
     return(
         <div className="home">
-            <h1 className="center purple-text text-darken-2">🤶Wicked Sales</h1>
+            <h1 className="center purple-text text-darken-2">Wicked Sales</h1>
         </div>
     )
 }
