5f6bfae028aae801a8eca5681ab54bcf98730011
Kylie Lin <kylieclin@gmail.com>
2019-04-05 15:22:49 -0700

    added initial carta add add error handling added update cart

--- public/api/addcartitem.php
+++ public/api/addcartitem.php
@@ -33,8 +33,38 @@ if(empty($cart_id)){
     `created`= NOW(), 
     `users_id`= $user_id,
     `changed`= NOW()";
+    $cart_result =mysqli_query($conn, $cart_create_query);
+    if(!$cart_result){
+        throw new Exception(mysqli_rror($conn));
+    }
+    if(mysqli_affected_rows($conn) === 0){
+        throw new Exception('data was not added to cart table');
+    }
+    $cart_id = mysqli_insert_id($conn); //get insert id
 }
-print($cart_create_query);
 
+$cart_item_query = "INSERT INTO `cart_items` SET
+`products_id` = $product_id,
+`quantity` = $product_quantity,
+`carts_id` = $cart_id
+";
+
+$cart_item_result = mysqli_query($conn, $cart_item_query);
+
+if(!$cart_item_result){
+    throw new Exception(mysqli_error($conn));
+}
+
+if(mysqli_affected_rows($conn) === 0){
+    throw new Exception('Did not add items to cart');
+}
+
+$output =[
+    'success' => true,
+    'cartCount'=>$product_quantity,
+    'cartTotal'=>$product_total
+];
+
+print(json_encode($output));
 
 ?>
