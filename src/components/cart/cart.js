69a93ab70e10cb6411d507ca6775026a3a3b2e92
Kylie Lin <kylieclin@gmail.com>
2019-04-10 18:13:02 -0700

    added cart page demostrate items, price, picture in cart page formated prices

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,17 +1,74 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
-import { async } from 'q';
+import { formatMoney } from '../../helpers';
+import './cart.scss';
 
 class Cart extends Component{
-    getCartData = async ()=>{
-        //const resp = await axios.get('/api/getcartitem/php')
+    constructor(props){
+        super(props);
+
+        this.state ={
+            items:[],
+            meta:{}
+        }
+    }
+    componentDidMount(){
+        this.getCartData();
+    }
+    async getCartData(){
+        const {data={}} = await axios.get('/api/getcartitems.php');
+
+        if(data.success){
+            this.setState({
+                items: data.cartItems,
+                meta: data.cartMetaData
+            })
+        } else {
+            console.error('cart data fail to load');
+        }
     }
     render(){
+        console.log(this.state);
+        const {items, meta} = this.state;
+        let totalItems = 0;
+
+        const cartItems = items.map(({name, price, image , quantity, id}) =>{
+            totalItems += quantity;
+            const itemtotalPrice = formatMoney(price* quantity);
+            return(
+                <tr key={id}>
+                    <td><img className="cartimg" src={`/dist/${image}`} alt={`${image} product image`}/></td>
+                    <td>{name}</td>
+                    <td>{formatMoney(price)}</td>
+                    <td>{quantity}</td>
+                    <td>{itemtotalPrice}</td>
+                </tr>
+            )
+        })
+
         return(
-            <div>
-                <h1>CART</h1>
-                <Link to="/products">Continue Shopping</Link>
+            <div className="cart">
+                <h1 className="center">Ice-cream Cart</h1>
+                <Link to="/products" className="continue-shopping">Continue Shopping</Link>
+                <div className="total-items">{totalItems} items in the cart</div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Item Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartItems}
+                        <tr>
+                            <td colSpan="5" className="total-price">Total: {formatMoney(meta.total)}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
